103649b00548e4d98b93b36c401ddc8a6dabf848
john-abo <john.abo@ucalgary.ca>
2021-09-03 13:06:02 -0600

    Fixed client side request reading
    Finally able to properly read the request sent by the user, and send a proper message back that will be written to the text box.

--- src/serv.js
+++ src/serv.js
@@ -3,16 +3,22 @@
  * side page's response
  */
 
-//Testing express js for use in project
+//Modules used in the project
 const { randomInt } = require('crypto');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 
+//Parses requests into a json
+app.use(express.json());
+
+//Sets port that will be listened to
 const port = 3000;
 
-app.listen(port, ()=>{
+//Begins listening on port set. Server is started
+app.listen(port, (err) => {
+    if (err) console.log(err);
     console.log('Server is listening on port: ' + port);
 });
 
@@ -21,18 +27,22 @@ app.get('/', (req, res) => {
 
     console.log('req received GET');
 
-    res.sendFile(path.resolve(__dirname,'./pages/test.html'));
+    res.sendFile(path.resolve(__dirname, './pages/test.html'));
 });
 
 // Handles post request made by user
 app.post('/test', (req, res) => {
 
-    console.log('req received POST: \n' + (req.body));
-    
+    //Prints msg to console
+    const msg = req.body.message;
+    console.log('req received POST: \n' + (msg));
+
     const writeStream = fs.createWriteStream('./pages/logs.txt');
     const readStream = fs.createReadStream('./pages/logs.txt');
 
-    res.write('Post ID ' + randomInt(10,99));
+    //TODO change this so that the data from a file is written to
+    //the response. and implement recent/log files
+    res.write('Post ID ' + randomInt(100, 999) + ': ' + msg);
 
     readStream.pipe(res);
     res.send();
@@ -41,6 +51,6 @@ app.post('/test', (req, res) => {
 });
 
 // Handles all other get requests
-app.all('*', (req,res)=>{
+app.all('*', (req, res) => {
     res.status(404).send('Not found...')
 });
